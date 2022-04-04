3a401e772b7f7925b3522c9bcef9ea8321a92ec2
iiHerilo <aaron.rogers.ok@gmail.com>
2022-04-04 15:31:21 -0500

    Make canvas size dynamic

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function init() {
+    element(ID.main).style.height = element(ID.main).getAttributeNode('height').value;
+    element(ID.main).style.width = element(ID.main).getAttributeNode('width').value;
+    window.addEventListener('resize', clamp)
+    clamp();
     console.info("CONFIG: %o", CFG);
     max = CFG.default_array_length;
     data = sequence(0, max);
@@ -17,6 +21,8 @@ function init() {
     element("version").innerHTML = CFG.version;
     halt(CFG.wait_time);
     draw();
+    console.info(window.innerHeight);
+    console.info(window.innerWidth)
 }
 
 function draw() {
@@ -62,3 +68,24 @@ function draw() {
 
     window.requestAnimationFrame(draw);
 }
+
+function clamp() {
+    var wh = window.innerHeight;
+    var ww = window.innerWidth;
+    var calcHeight = window.getComputedStyle(element(ID.main)).getPropertyValue('height');
+    calcHeight = parseInt(calcHeight.substring(0, calcHeight.length-2));
+
+    if (wh < ww) {
+        element(ID.main).style.height = `${wh - 300}px`;
+        element(ID.main).style.width = `${wh - 300}px`;
+        console.log(`${wh - 300}px`)
+    }
+    else if (calcHeight > ww){
+        element(ID.main).style.height = `auto`;
+        element(ID.main).style.width = `auto`;
+    }
+
+    element();
+}
+
+window.onresize = clamp;
